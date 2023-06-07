e1909f44605698e5a914e5197c342cb6a448f932
Sunse7 <55135188+Sunse7@users.noreply.github.com>
2023-06-07 20:16:37 +0200

    Validation on add menu item

--- routes/menuRoute.js
+++ routes/menuRoute.js
@@ -16,15 +16,28 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/add', async (req, res) => {
-    const { title, desc, price } = req.body;
-    const menuItem = {
-        title: title,
-        desc: desc,
-        price: price,
-        createdAt: new Date()
+    const body = req.body;
+
+    //TODO: Make middleware
+    if(body.hasOwnProperty('title') && body.hasOwnProperty('desc') && body.hasOwnProperty('price')) {
+
+        if(body.title.length > 3 && body.desc.length > 3 && body.price > 0){
+            const menuItem = {
+                title: body.title,
+                desc: body.desc,
+                price: body.price,
+                createdAt: new Date().toISOString()
+            }
+            const data = await addNewMenuItem(menuItem);
+            res.status(201).json({ success: true, data: data });
+        } else {
+            res.status(400).json({ success: false, message: 'Fields can not be empty' });
+        }
+
+    } else {
+        res.status(400).json({ success: false, message: 'Must contain title, desc and price' });
     }
-    const data = await addNewMenuItem(menuItem);
-    res.json({ success: true, data: data })
+
 });
 
 router.put('/update/:id', async (req, res) => {
@@ -34,7 +47,6 @@ router.put('/update/:id', async (req, res) => {
 router.delete('/delete/:id', async (req, res) => {
     const id = req.params.id;
     const foundItem = await findMenuItemById(id); // Gets one item
-    // console.log('found item', foundItem);
 
     if(foundItem) { 
         await deleteMenuItemById(id);
