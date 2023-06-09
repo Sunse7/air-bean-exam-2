237f2715830079af0f2e6e61eef318e5b45aee3c
Sunse7 <55135188+Sunse7@users.noreply.github.com>
2023-06-09 13:17:37 +0200

    Token and admin permissions works

--- middlewares/jwt.js
+++ middlewares/jwt.js
@@ -2,25 +2,28 @@ const jwt = require('jsonwebtoken');
 const secretKey = 'a1b1c1';
 
 function generateToken(payload) {
-    console.log('This');
     const token = jwt.sign(payload, secretKey, { expiresIn: '1h' });
-    console.log('Generate token', token);
     return token;
 }
 
 function verifyToken(allowedRoles) {
     return (req, res, next) => {
-    const token = req.headers.authorization.replace('Bearer ', '');
-    const verifiedToken = jwt.verify(token, secretKey);
-        if(verifiedToken) {
-            if(allowedRoles.includes(verifiedToken.role)) {
-                next();
+        try {
+            const token = req.headers.authorization.replace('Bearer ', '');
+            const verifiedToken = jwt.verify(token, secretKey);
+             if(verifiedToken) {
+                if(verifiedToken.role == allowedRoles) {
+                    next();
+                } else {
+                    res.status(403).json({ success: false, message: 'Missing permission for access' });
+                }
             } else {
-                res.status(403).json({ success: false, message: 'Missing permission for access' });
+                res.status(400).json({ success: false, message: 'Invalid token'});
             }
-        } else {
-            res.status(400).json({ success: false, message: 'Invalid token'});
+        } catch (err) {
+            res.status(500).json({ success: false, message: 'Invalid token'})
         }
+    
     }
 }
 
